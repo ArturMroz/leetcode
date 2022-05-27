d448f2d965b66b41aa4cc61bfd568369377ce288
Artur Mroz <frost47@gmail.com>
2022-05-27 08:29:05 +0100

    add another solution to 3. Longest Substring

--- 0003_longest_substring_without_repeating_characters.go
+++ 0003_longest_substring_without_repeating_characters.go
@@ -11,15 +11,37 @@ func lengthOfLongestSubstring(s string) int {
 
 	for i := range s {
 		if v, ok := seen[s[i]]; ok {
-			for j := l; j <= v; j++ {
+			for j := l; j < v; j++ {
 				delete(seen, s[j])
-				l++
 			}
+			l = v + 1
 		}
 
 		seen[s[i]] = i
 		if len(seen) > max {
 			max = len(seen)
+		}
+	}
+
+	return max
+}
+
+// time O(n), space O(n)
+func lengthOfLongestSubstring_flat(s string) int {
+	max := 0
+	seen := map[byte]struct{}{}
+	l, r := 0, 0
+
+	for l < len(s) && r < len(s) {
+		if _, ok := seen[s[r]]; ok {
+			delete(seen, s[l])
+			l++
+		} else {
+			seen[s[r]] = struct{}{}
+			r++
+			if r-l > max {
+				max = r - l
+			}
 		}
 	}
 
